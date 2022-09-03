4d7540d3e6a0e9820232d6fc54f5ffee66efbda5
AkenoHimejimaa <shebyowo@gmail.com>
2022-09-03 02:53:04 -0400

    a

--- Core/Utility Ports/EvadeSharp/Config.cs
+++ Core/Utility Ports/EvadeSharp/Config.cs
@@ -15,7 +15,7 @@ namespace Evade
     {
         public const bool PrintSpellData = false;
         public const bool Debug = false;
-        public const bool TestOnAllies = true;
+        public const bool TestOnAllies = false;
         public const int SkillShotsExtraRadius = 13;
         public const int SkillShotsExtraRange = 20;
         public const int GridSize = 10;
